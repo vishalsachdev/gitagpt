dee22fee8ce85ab6dd3e7fe0a326c2d24274cacf
Vishal Sachdev <svishal2001@gmail.com>
2023-01-29 13:25:50 -0600

    update file name

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ chain = VectorDBQAWithSourcesChain.from_llm(llm=OpenAI(temperature=0), vectorsto
 
 
 # From here down is all the StreamLit UI.
-st.set_page_config(page_title="Blendle Notion QA Bot", page_icon=":robot:")
-st.header("Blendle Notion QA Bot")
+st.set_page_config(page_title="Bhagvad Geeta Question Answer Bot", page_icon=":robot:")
+st.header("Bhagvad Geeta Question Answer Bot")
 
 if "generated" not in st.session_state:
     st.session_state["generated"] = []
