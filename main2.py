5aad5e1eb231c6fe4b38c8f4a17de046d24957a4
Vishal Sachdev <svishal2001@gmail.com>
2023-01-29 19:36:35 -0600

    indent

--- main2.py
+++ main2.py
@@ -48,8 +48,8 @@ output=""
 if user_input:
     # Call the chain function with the user input as the question
     with st.spinner('Wait for it...'):
-    time.sleep(5)
-    st.success('Done!')
+        time.sleep(5)
+        st.success('Done!')
     result = chain({"question": user_input})
     # Format the answer and sources into a string
     output = f"Answer: {result['answer']}"
