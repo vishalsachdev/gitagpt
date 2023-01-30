3bd2dd2318460a0d17c76893123213083926a8d5
Vishal Sachdev <svishal2001@gmail.com>
2023-01-30 11:38:26 -0600

    removed the spinner

--- main2.py
+++ main2.py
@@ -47,8 +47,6 @@ output=""
 # Check if the user input is not None
 if user_input:
     # Call the chain function with the user input as the question
-    while not (result):
-        st.spinner('Getting a response from the bot...')  
     result = chain({"question": user_input})
        
     
